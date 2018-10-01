03048d051cae65d8eedcfb486d6f76268258f460
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-01 14:24:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
  
 # New Code!!
-  before_save :email_author_about_post
+#before_save :email_author_about_post
  
   private
  
