c3c5e820ef40c7b7099f513d6215ffa342dd4929
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-01 14:18:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,19 @@
 class Post < ActiveRecord::Base
-
+ 
   belongs_to :author
   validate :is_title_case 
-
+ 
+# New Code!!
+  before_save :make_title_case 
+ 
   private
-
+ 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
   end
-
+ 
   def make_title_case
     self.title = self.title.titlecase
   end
